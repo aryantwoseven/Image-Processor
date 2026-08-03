5876251cdc36f71eecae6a7367ff87d9370899fc
Aryan <aryansingh2275@gmail.com>
2026-08-04 02:32:34 +0530

    dockerfile

--- webfilter/main.go
+++ webfilter/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("POST /filters/{name}", processHandler)
 	mux.HandleFunc("GET /health", healthHandler)
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 
-	log.Println("listening on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("listening on :" + port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatal(err)
 	}
 }
